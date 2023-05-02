cfa000117d44810b089e7fc2d3c188191bb2a134
Ulie123 <123515813+Ulie123@users.noreply.github.com>
2023-05-02 17:08:40 -0400

    PLayer stats

--- se-practice/src/main/java/project/Player.java
+++ se-practice/src/main/java/project/Player.java
@@ -1,4 +1,43 @@
 package project;
 
 public class Player {
+    //each player has a Name, ID, Overall batting stats, Game batting stats.
+    private String name;
+    private int ID;
+    //player ID
+    private int GP;
+    //Games Played
+    private int AB;
+    //At Bats
+    private int R;
+    //Runs
+    private int H;
+    //Hits
+    private int D;
+    //Doubles
+    private int T;
+    //Triples
+    private int HR;
+    //Home Runs
+    private int RBI;
+    //Runs Batted In
+    private int TB;
+    //Total Bases
+    private int BB;
+    //Walks
+    private int K;
+    //Strikeouts
+    private int SB;
+    //Stolen Bases
+    private long AVG;
+    //Batting Average
+    private long OBP;
+    //On Base Percentage
+    private long SLG;
+    //Slugging Percentage
+    private long OPS;
+    //On Base plus Slugging
+    private long WAR;
+    //Wins Above Replacement
+
 }
