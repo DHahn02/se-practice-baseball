d75d68acb243537984350aa221cf87b39c738f90
DHahn02 <dhahn@mail.yu.edu>
2023-05-04 17:37:15 -0400

    cc

--- se-practice/src/main/java/project/CommandCenter.java
+++ se-practice/src/main/java/project/CommandCenter.java
@@ -1,5 +1,19 @@
 package project;
+import Calendar;
 
 public class CommandCenter {
+
+    public void addAtBat(Player p, AtBat a, Game g){
+
+    }
+
+
+    private String getDateInt(){
+        String year = now.getDate();
+        String month = now.getMonth();
+        String day = now.getDay();
+        return year+month+day;
+    }
+
 
 }
