ba25861d9a7a4d55e3af1cc80bd08e6e2f83859d
DHahn02 <dhahn@mail.yu.edu>
2023-05-04 18:04:36 -0400

    heaps

--- se-practice/src/main/java/project/CommandCenter.java
+++ se-practice/src/main/java/project/CommandCenter.java
@@ -1,9 +1,24 @@
 package project;
 import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class CommandCenter {
+    PriorityQueue<Player> lineup;
+    PriorityQueue<Player> demotions;
+
+    public CommandCenter(){
+        this.lineup = new PriorityQueue<>(Comparator.reverseOrder());
+        this.demotions = new PriorityQueue<>();
+    }
 
     public void addAtBat(Player p, AtBat a, Game g){
+
+    }
+
+    public List<Player> getLineup(){
 
     }
 
