fec88467471718776b6e672284b98a5f7a52600c
DHahn02 <dhahn@mail.yu.edu>
2023-05-04 18:07:01 -0400

    players

--- se-practice/src/main/java/project/CommandCenter.java
+++ se-practice/src/main/java/project/CommandCenter.java
@@ -1,17 +1,16 @@
 package project;
 import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class CommandCenter {
     PriorityQueue<Player> lineup;
     PriorityQueue<Player> demotions;
+    Map<String, Player> players;
 
     public CommandCenter(){
         this.lineup = new PriorityQueue<>(Comparator.reverseOrder());
         this.demotions = new PriorityQueue<>();
+        this.players = new HashMap<>();
     }
 
     public void addAtBat(Player p, AtBat a, Game g){
